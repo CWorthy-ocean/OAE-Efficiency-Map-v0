43ecaf915f7a64f444968de2a94ece305ca301e2
matt-long <mclong@ucar.edu>
2023-05-12 06:32:41 -0600

    init

---
+++ notebooks/config.py
@@ -0,0 +1,14 @@
+import os
+
+USER = os.environ['USER']
+project_name = 'oae-dor-global-efficiency'
+
+account = "P93300670"
+
+dir_project_root = f'/glade/work/{USER}/{project_name}'
+os.makedirs(dir_project_root, exist_ok=True)
+
+dir_data = f'{dir_project_root}/data'
+os.makedirs(dir_data, exist_ok=True)
+
+dir_scratch = f'/glade/scratch/{USER}'
